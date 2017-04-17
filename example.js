16f4b0381fb803219aee3b16c7d92162c566f3bb
Andreas Holstenson <a@holstenson.se>
2017-04-17 17:42:07 +0200

    Example does not need a device model

--- example.js
+++ example.js
@@ -4,7 +4,6 @@ const miio = require('./lib');
 
 // Create a new device over the given address
 miio.device({
-	model: 'zhimi.airpurifier.m1',
 	address: '192.168.100.8'
 }).then(device => {
 	console.log('power is now', device.power);
