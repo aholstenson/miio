aab89b1d8ff986d9b0cfed62e9eb5bd9a9ad5070
Andreas Holstenson <a@holstenson.se>
2017-04-17 17:35:45 +0200

    Updating example to match newer API

--- example.js
+++ example.js
@@ -3,20 +3,10 @@
 const miio = require('./lib');
 
 // Create a new device over the given address
-const device = miio.createDevice({
+miio.device({
 	model: 'zhimi.airpurifier.m1',
 	address: '192.168.100.8'
-});
-
-// Call any method via call
-device.call('set_mode', [ 'silent' ])
-	.then(console.log)
-	.catch(console.error);
-
-
-// Or use some of the built-in methods
-device.setPower(false);
-
-device.getProperties([ 'power', 'mode', 'aqi', 'temp_dec', 'humidity' ])
-	.then(console.log)
-	.catch(console.error);
+}).then(device => {
+	console.log('power is now', device.power);
+	return device.setPower(! device.power);
+}).catch(console.error);
