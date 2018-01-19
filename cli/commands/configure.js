0976e185a2dee066d24232c6dd9b20b5b82edcba
Andreas Holstenson <a@holstenson.se>
2018-01-19 09:56:51 +0100

    Adding configure to CLI to support updating wireless

---
+++ cli/commands/configure.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const chalk = require('chalk');
+const log = require('../log');
+const deviceFinder = require('../device-finder');
+
+const tokens = require('../../lib/tokens');
+
+exports.command = 'configure <idOrIp>';
+exports.description = 'Control a device by invoking the given method';
+exports.builder = {
+	ssid: {
+		required: true,
+		description: 'SSID of the WiFi network'
+	},
+
+	passwd: {
+		required: true,
+		description: 'Password of WiFi-network'
+	}
+};
+
+exports.handler = function(argv) {
+	let target = argv.idOrIp;
+	log.info('Attempting to configure', target);
+
+	let foundDevice = false;
+	let pending = 0;
+	const browser = deviceFinder({
+		filter: target
+	});
+	browser.on('available', device => {
+		pending++;
+
+		log.plain();
+
+		device.management.updateWireless({
+			ssid: argv.ssid,
+			passwd: argv.passwd
+		})
+			.then(result => {
+				log.plain('Updated wireless configuration');
+
+				return tokens.update(device.id, device.management.token);
+			})
+			.catch(err => {
+				log.error('Encountered an error while updating wireless');
+				log.plain();
+				log.plain('Error was:');
+				log.plain(err.message);
+			})
+			.then(() => {
+				pending--;
+				process.exit(0);
+			});
+	});
+
+	const doneHandler = () => {
+		if(pending == 0) {
+			if(! foundDevice) {
+				log.warn('Could not find device');
+			}
+			process.exit(0);
+		}
+	};
+	setTimeout(doneHandler, 5000);
+	browser.on('done', doneHandler);
+};
